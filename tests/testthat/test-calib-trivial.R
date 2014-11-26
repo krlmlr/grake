215ba26bae4c2915f9e6c59a71f3573dff3b5414
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-11-26 18:54:25 +0100

    add trivial tests, currently failing

---
+++ tests/testthat/test-calib-trivial.R
@@ -0,0 +1,19 @@
+context('Trivial')
+
+test_that('Calibrating a unit matrix against a unit vector', {
+  N <- 10
+  X <- diag(N)
+  d <- rep(1, N)
+  totals <- rep(1, N)
+  g <- calibWeights(X, d, totals)
+  expect_equal(g, totals)
+})
+
+test_that('Calibrating a unit matrix against a unit vector with random totals', {
+  N <- 10
+  X <- diag(N)
+  d <- rep(1, N)
+  totals <- runif(N)
+  g <- calibWeights(X, d, totals)
+  expect_equal(g, totals)
+})
