dccc0ac0da763d64bc7a1c778ff08ab332cf6c88
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-21 21:31:39 +0100

    add test for non-convergence

--- tests/testthat/test-calib-trivial.R
+++ tests/testthat/test-calib-trivial.R
@@ -22,3 +22,25 @@ test_that('Calibrating a unit matrix against a vector with random totals', {
     expect_equal(g, totals, info = method)
   }
 })
+
+
+test_that('Test non-convergence', {
+  N <- 10
+  X <- diag(N)
+  X[1,2] <- 1
+  d <- rep(1, N)
+  totals <- c(2, rep(1, N - 1))
+
+  for (method in eval(formals(calibWeights)$method)) {
+    if (method == "linear") {
+      g <- calibWeights(X, d, totals, method = method)
+      expect_equal(as.vector(g %*% X), totals, info = method)
+      expect_true(any(g < 0))
+    } else {
+      expect_warning(
+        g <- calibWeights(X, d, totals, method = method),
+        "no convergence")
+      expect_equal(g, NULL)
+    }
+  }
+})
