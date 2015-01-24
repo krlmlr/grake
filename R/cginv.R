d919e7190ef03b845209122eaca3c1a6c5152de2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-24 01:13:00 +0100

    need braces for proper coverage analysis

--- R/cginv.R
+++ R/cginv.R
@@ -26,16 +26,20 @@
 #'
 #' @export
 cginv <- function(X, tol = sqrt(.Machine$double.eps), svd = NULL) {
-  if (is.null(svd))
+  if (is.null(svd)) {
     svd <- base::svd
+  }
 
-  if (length(dim(X)) > 2L || !(is.numeric(X) || is.complex(X)))
+  if (length(dim(X)) > 2L || !(is.numeric(X) || is.complex(X))) {
     stop("'X' must be a numeric or complex matrix")
-  if (!is.matrix(X))
+  }
+  if (!is.matrix(X)) {
     X <- as.matrix(X)
+  }
   Xsvd <- svd(X)
-  if (is.complex(X))
+  if (is.complex(X)) {
     Xsvd$u <- Conj(Xsvd$u)
+  }
   Positive <- Xsvd$d > max(tol * Xsvd$d[1L], 0)
   if (all(Positive)) {
     Xsvd$v %*% (1/Xsvd$d * t(Xsvd$u))
