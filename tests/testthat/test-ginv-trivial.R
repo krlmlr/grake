e2f2a07c836ddc9b9df8e254cab9b9df87a5ec8b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-09 21:25:20 +0200

    check corner cases

--- tests/testthat/test-ginv-trivial.R
+++ tests/testthat/test-ginv-trivial.R
@@ -7,6 +7,8 @@ test_that('Compare ginv results', {
   cols <- 19L
   X <- matrix(runif(rows * cols), rows, cols)
   expect_equal(MASS::ginv(X), cginv(X))
+  expect_equal(MASS::ginv(X, tol = 1), cginv(X, tol = 1))
+  expect_equal(MASS::ginv(X, tol = 1), cginv(X, tol = 1))
   X <- matrix(complex(real = runif(rows * cols), imag = runif(rows * cols)),
               rows, cols)
   expect_equal(MASS::ginv(X), cginv(X))
